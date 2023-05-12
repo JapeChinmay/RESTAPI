3de432cf8d0c0e82d2f53bfd18ea401faece7394
JpaeChinmay <iam95041@gmail.com>
2023-05-12 23:31:56 +0530

    added first middleware,

--- Server/app.js
+++ Server/app.js
@@ -4,6 +4,17 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("fro mthe middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.time = new Date().toISOString();
+  next();
+});
+//middleware placing matters
+
 const Tours = [
   {
     post: "somedata",
@@ -11,20 +22,13 @@ const Tours = [
   },
 ];
 
-app.get("/api/v1/tours", (req, res) => {
+const getTours = (req, res) => {
   console.log(Tours);
 
-  return res.status(200).json(Tours);
-});
+  return res.status(200).json({ time: req.time });
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
-  console.log(req.params);
-
-  return res.status(200).json({ message: "ok" });
-  //getting id , in params
-});
-
-app.post("/api/v1/tours", (req, res) => {
+const postTours = (req, res) => {
   console.log(req.body);
   const newID = Tours.length + 1;
   const newTour = Object.assign({ ID: newID }, req.body);
@@ -33,16 +37,49 @@ app.post("/api/v1/tours", (req, res) => {
   console.log(Tours);
 
   return res.status(200).json({ message: "success" });
-});
+};
 
-app.patch("/api/v1/tours/:id", (req, res) => {
+const getTourbyID = (req, res) => {
+  console.log(req.params);
+
+  return res.status(200).json({ message: "ok" });
+  //getting id , in params
+};
+
+const patchTour = (req, res) => {
   const idNum = req.params.id;
   if (Number(idNum) > Tours.length) {
     return res.status(404).json({ message: "invalid id" });
   } else {
     return res.status(200).json({ message: "ok" });
   }
-});
+};
+
+const deleteTour = (req, res) => {
+  const idNum = req.params.id;
+  if (Number(idNum) > Tours.length) {
+    return res.status(404).json({ message: "invalid id" });
+  } else {
+    return res.status(204).json({ message: "NULL" });
+  }
+};
+
+app.route("/api/v1/tours").get(getTours).post(postTours);
+app
+  .route("/api/v1/tours/:id")
+  .get(getTourbyID)
+  .patch(patchTour)
+  .delete(deleteTour);
+
+//app.get("/api/v1/tours", getTours);
+
+// /app.get("/api/v1/tours/:id", getTourbyID);
+
+//app.post("/api/v1/tours", postTours);
+
+//app.patch("/api/v1/tours/:id", patchTour);
+
+//app.delete("/api/v1/tours/:id", deleteTour);
 
 app.listen(3000, () => {
   console.log("listening");
