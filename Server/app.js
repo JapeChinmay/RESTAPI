2707c10b014b848f7171da38f84965e6fe4a20f9
JpaeChinmay <iam95041@gmail.com>
2023-05-12 23:11:54 +0530

    added pathc post routes,

--- Server/app.js
+++ Server/app.js
@@ -2,13 +2,46 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+const Tours = [
+  {
+    post: "somedata",
+    ID: "122",
+  },
+];
 
 app.get("/api/v1/tours", (req, res) => {
-  const ans = {
-    name: "chinmay",
-  };
-  console.log(ans);
-  return res.status(200).json(ans);
+  console.log(Tours);
+
+  return res.status(200).json(Tours);
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params);
+
+  return res.status(200).json({ message: "ok" });
+  //getting id , in params
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  const newID = Tours.length + 1;
+  const newTour = Object.assign({ ID: newID }, req.body);
+
+  Tours.push(newTour);
+  console.log(Tours);
+
+  return res.status(200).json({ message: "success" });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const idNum = req.params.id;
+  if (Number(idNum) > Tours.length) {
+    return res.status(404).json({ message: "invalid id" });
+  } else {
+    return res.status(200).json({ message: "ok" });
+  }
 });
 
 app.listen(3000, () => {
